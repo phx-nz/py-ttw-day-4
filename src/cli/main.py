5fe95798d16ab6b1f21d5220cbd2881d3dd01648
Phoenix Zerin <todofixthis@users.noreply.github.com>
2023-11-13 12:45:51 +1300

    Install entry points 🔌

--- src/cli/main.py
+++ src/cli/main.py
@@ -8,10 +8,10 @@ Refer to README.rst for instructions to run commands via the CLI.
 __all__ = ["app"]
 
 import asyncio
+from importlib.metadata import entry_points
 
 import typer
 import uvloop
-
 from cli.commands import generate, profiles
 
 # Activate uvloop for improved asyncio performance.
@@ -24,6 +24,17 @@ app = typer.Typer()
 # Register commands so that they can be invoked.
 app.add_typer(generate.app)
 app.add_typer(profiles.app)
+
+# Register additional commands from plugins.
+for e in entry_points(group="app.command"):
+    plugin: typer.Typer = e.load()
+
+    if not isinstance(plugin, typer.Typer):
+        raise TypeError(
+            f"Invalid plugin {e.name} ({type(plugin).__name__}); typer.Typer expected"
+        )
+
+    app.add_typer(plugin, name=e.name)
 
 if __name__ == "__main__":
     app()
